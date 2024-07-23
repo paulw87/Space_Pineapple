dc476f8ba994d1312c609b8ba6e5f2a87d4ec984
gazaulk <paulwoodcock87@gmail.com>
2024-07-23 22:34:52 +0100

    started the upgrades added it as a view and the link button

--- Scenes/user_interface.gd
+++ Scenes/user_interface.gd
@@ -7,15 +7,23 @@ extends Control
 enum Views {
 	PROTOTYPE_AUTO_PINEAPPLE,
 	PROTOTYPE_CLICKER,
+	PROTOTYPE_UPGRADES,
 }
+
 
 ##emitted when something has requested navigation. includes the view target
 signal navigation_requested(view : Views)
+
 
 ##triggered when the auto pineapple link is clicked
 func _on_prototype_auto_pineapple_link_pressed() -> void:
 	navigation_requested.emit(Views.PROTOTYPE_AUTO_PINEAPPLE)
 
+
 ##triggered when the pineapple clicker link is clicked
 func _on_prototype_clicker_link_pressed() -> void:
 	navigation_requested.emit(Views.PROTOTYPE_CLICKER)
+
+
+func _on_prototype_upgrades_link_button_pressed():
+	navigation_requested.emit(Views.PROTOTYPE_UPGRADES)
